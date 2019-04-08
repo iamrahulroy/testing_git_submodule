9e0d2d8da6fedca3a213fb95146256a8e460c230
Rahul Roy <techniciablog@gmail.com>
2019-04-08 18:46:17 +0530

    Remove app models - preparing for submodule

--- app/models/comment.rb
+++
@@ -1,5 +0,0 @@
-class Comment < ApplicationRecord
-  def dummy_method
-    puts 'Dummy text'
-  end
-end
--- app/models/post.rb
+++
@@ -1,5 +0,0 @@
-class Post < ApplicationRecord
-  def self.first_two
-    first(2)
-  end
-end
